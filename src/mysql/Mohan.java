6960a3daa096a673fb584b46e572a3b9e58c784c
Mohan <Mohan@Mohan>
2018-05-27 14:43:10 +0530

    MOhan firstproject

---
+++ src/mysql/Mohan.java
@@ -0,0 +1,23 @@
+package mysql;
+
+import java.sql.DriverManager;
+
+import com.mysql.jdbc.ResultSet;
+import com.mysql.jdbc.Statement;
+
+public class Mohan {
+
+	public static void main(String[] args) throws Exception{
+java.sql.Connection con=DriverManager.getConnection("jdbc:odbc:login");
+java.sql.Statement st=con.createStatement();
+java.sql.ResultSet rs=st.executeQuery("select * from mohan");
+while(rs.next()) {
+	System.out.println(rs.getInt(0)+"__________________"+rs.getInt(1));
+}
+
+
+
+
+	}
+
+}
